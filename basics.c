37fdcdac77cf10c4766e01f919e6eda6f0b16a07
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2025-01-16 18:59:41 +0530

    Create basics.c

---
+++ basics.c
@@ -0,0 +1,57 @@
+
+#include <stdio.h>
+//header file - stdio.h - Standard Input Output
+
+int main() {
+// int main() - main method/function - heart of the program
+  
+    // int x = 15;
+    // int y = 20;
+    // printf("The Sum of %d and %d is %d",x,y,x+y);
+    // int x = 20;
+    // printf("%d\n",x);
+    // float y = 5.345;
+    // printf("%.2f",y);
+        // double a = 12345.6789;
+        // printf("%.4lf",a);
+    // long num = 12345678;
+    // printf("%ld",num);
+    
+    // long long num = 1234567891011121314;
+    // printf("%lld",num);
+    
+    // char ch = 'A';
+    // printf("%c",ch);
+    
+    // int num1,num2;
+    // printf("Enter No1:");
+    // scanf("%d",&num1);
+    // printf("Enter No2:");
+    // scanf("%d",&num2);
+    
+    // if (num1>num2)
+    // {
+    //     printf("%d is largest",num1);
+    // }
+    // else{
+    //     printf("%d is largest",num2);
+    // }
+    // printf("The Sum of %d and %d is %d",num1,num2,num1+num2);
+    // printf("%d",27%7); 
+    
+    int p;
+    printf("enter no:");
+    scanf("%d",&p);
+    // Conditional Statements
+    if (p%2==0)
+    {
+        printf("%d is even",p);
+    }
+    else{
+        printf("%d is odd",p);
+    }
+        
+        
+        
+    return 0;
+}
