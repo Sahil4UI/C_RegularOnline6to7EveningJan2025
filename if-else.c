f8289ef6c612afc0e8cef6cd47b8455c9382b736
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2025-01-17 19:00:57 +0530

    Create if-else.c

---
+++ if-else.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+int main()
+{
+    
+//   if -else - ladder if else
+//   int x,y,z;
+//   printf("Enter No 1: ");
+//   scanf("%d",&x);
+//   printf("Enter No 2: ");
+//   scanf("%d",&y);
+//   printf("Enter No 3: ");
+//   scanf("%d",&z);
+   
+// //   find the largest no 
+//   if (x>y && x>z)
+//   {
+//       printf("%d is largest",x);
+//   }
+//   else if (y>z && y>x)
+//   {
+//       printf("%d is largest",y);   
+//   }
+//   else{
+//       printf("%d is largest",z);
+//   }
+//   you have 3 sides,if they can form a traingle.check which traingle is it?
+// isoceles/scalene/equilateral
+
+//   int x,y,z;
+//   printf("Enter Side 1: ");
+//   scanf("%d",&x);
+//   printf("Enter Side 2: ");
+//   scanf("%d",&y);
+//   printf("Enter Side 3: ");
+//   scanf("%d",&z);
+// //   sum of two sides of traingle is always greater than the third side
+//   if (x+y>z && y+z>x && z+x>y)
+// {
+//               if (x==y && y==z)
+//           {
+//               printf("equilateral Traingle");
+//           }
+//           else if (x==y || y==z || z==x)
+//           {
+//               printf("Isoceles Traingle");
+//           }
+//           else{
+//               printf("Scalene Traingle");
+//           }
+// }  
+// else{
+//     printf("Traingle cannot be formed");
+// }
+    int x = 5;
+    // x++; //increment by one
+    x--;
+    printf("%d",x);
+    return 0;
+    
+}
